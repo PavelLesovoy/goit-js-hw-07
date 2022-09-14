3ab807bab2c88dd3ce33cf00270b7e545e811742
PavelLesovoy <98907227+PavelLesovoy@users.noreply.github.com>
2022-09-14 19:24:14 +0300

    update

--- js/task-01.js
+++
@@ -1,7 +0,0 @@
-const categoriesElements = document.querySelectorAll('.item');
-console.log('Number of categories:', categoriesElements.length);
-
-categoriesElements.forEach(function(categories){
-    console.log("Category:",categories.firstElementChild.textContent);
-    console.log("Elements:",categories.lastElementChild.children.length);
-})
