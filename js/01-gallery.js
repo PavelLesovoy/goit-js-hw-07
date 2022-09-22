8116b50a5c4a94e47d63ac83b1faa17742a90910
PavelLesovoy <98907227+PavelLesovoy@users.noreply.github.com>
2022-09-22 19:54:24 +0300

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -60,29 +60,3 @@ function galleryItemsOn(event) {
 
 console.log(galleryItems);
 
-
-import { galleryItems } from "./gallery-items.js";
-
-const listGallery = document.querySelector(".gallery");
-
-const linksGallery = galleryItems
-  .map(({ preview, original, description }) => {
-    return `
-         <a class="gallery__item" href="${original}">
-           <img
-             class="gallery__image"
-             src="${preview}"
-             alt="${description}"
-           />
-         </a>
-         `;
-  })
-  .join(" ");
-
-listGallery.insertAdjacentHTML("beforeend", linksGallery);
-
-let galleryLightbox = new SimpleLightbox(".gallery a", {
-  captionsData: "alt",
-  captionPosition: "bottom",
-  captionDelay: 250,
-});
